1d62b670b97d43cb6784f2e256a1e662cab479ae
Neej03 <neej.butani1603@gmail.com>
2026-02-21 19:53:42 +0530

    my portfolio updated

---
+++ src/App.jsx
@@ -0,0 +1,33 @@
+import CustomCursor from "./components/CustomCursor";
+import NavBar from "./components/NavBar";
+import ParticlesBackground from "./components/ParticleBackground";
+import About from "./sections/About";
+import Contact from "./sections/Contact";
+import Experience from "./sections/Experience";
+import Footer from "./sections/Footer";
+import Home from "./sections/Home";
+import Projects from "./sections/Projects";
+import Skills from "./sections/Skills";
+import Testimonials from "./sections/Testimonials";
+
+
+export default function App() {
+  return (
+<div className='relative gradient text-white'>
+  <CustomCursor />
+<ParticlesBackground />
+
+<NavBar />
+<Home />
+<About />
+<Skills />
+<Projects />
+<Experience />
+<Testimonials />
+<Contact />
+<Footer />
+
+
+</div>
+  );
+}
